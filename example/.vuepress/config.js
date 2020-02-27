026953316d5b1c72b16c92635ca7b7fb50b6dc2d
xuxiaofei.666 <xuxiaofei.666@bytedance.com>
2020-02-27 18:08:24 +0800

    ga

--- example/.vuepress/config.js
+++ example/.vuepress/config.js
@@ -28,6 +28,10 @@ module.exports = {
           ]
         },
         html: true
+      },
+      '@vuepress/google-analytics',
+      {
+        'ga': 'UA-159222254-1' // UA-00000000-0
       }
     ]
   ]
